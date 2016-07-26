4d40171c54fec8b9d28b2f0f5f457e0063eb34df
blqw <fttl_398@126.com>
2016-07-26 23:13:32 +0800

    添加项目文件。

---
+++ blqw.Configuration/ConfigNodeType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blqw.Configuration
+{
+    /// <summary>
+    /// 配置节点类型
+    /// </summary>
+    public enum ConfigNodeType
+    {
+        Null = 0,
+        /// <summary>
+        /// 值
+        /// </summary>
+        Value = 1,
+        /// <summary>
+        /// 对象(键值对)
+        /// </summary>
+        Object = 2,
+        /// <summary>
+        /// 数组
+        /// </summary>
+        Array = 3,
+    }
+}
